dd5100904ecf9cf70dfe355a8576467818404fd1
Azeb Tesfay <azbseb@gmail.com>
2026-04-25 09:23:20 -0700

    test for testTimeToBurnOneTree

--- src/FireTest.java
+++ src/FireTest.java
@@ -6,10 +6,10 @@ public class FireTest {
     @Test
     public void testTimeToBurnExample() {
         char[][] forest = {
-            {'t','.','.','t','t','t','t','.','t'},
-            {'.','.','t','t','.','.','.','.','t'},
-            {'.','.','t','t','t','t','t','t','t'},
-            {'t','t','t','t','.','.','.','.','.'}
+                { 't', '.', '.', 't', 't', 't', 't', '.', 't' },
+                { '.', '.', 't', 't', '.', '.', '.', '.', 't' },
+                { '.', '.', 't', 't', 't', 't', 't', 't', 't' },
+                { 't', 't', 't', 't', '.', '.', '.', '.', '.' }
         };
 
         int matchR = 2;
@@ -21,20 +21,34 @@ public class FireTest {
         assertEquals(expected, actual);
     }
 
-
-        @Test
+    @Test
     public void testTimeToBurnSingleTree() {
         char[][] forest = {
-            {'.','.','.','.','.','.','.','.','.'},
-            {'.','.','.','.','.','.','.','.','.'},
-            {'.','.','.','t','.','.','.','.','.'},
-            {'.','.','.','t','.','.','.','.','.'}
+                { '.', '.', '.', '.', '.', '.', '.', '.', '.' },
+                { '.', '.', '.', '.', '.', '.', '.', '.', '.' },
+                { '.', '.', '.', 't', '.', '.', '.', '.', '.' },
+                { '.', '.', '.', 't', '.', '.', '.', '.', '.' }
         };
 
         int matchR = 3;
         int matchC = 3;
 
         int expected = 1;
+        int actual = Fire.timeToBurn(forest, matchR, matchC);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testTimeToBurnOneTree() {
+        char[][] forest = {
+                { 't' }
+        };
+
+        int matchR = 0;
+        int matchC = 0;
+
+        int expected = 0;
         int actual = Fire.timeToBurn(forest, matchR, matchC);
 
         assertEquals(expected, actual);
